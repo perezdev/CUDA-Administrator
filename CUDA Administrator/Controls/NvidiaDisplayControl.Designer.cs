ce96153ec1f311038102d3e0ac93527ae925f934
perezdev <ernest.perezjr@gmail.com>
2014-03-30 20:10:35 -0500

    Fixed issue where NvidiaDisplayControl didn't save to last commit // ep

---
+++ CUDA Administrator/Controls/NvidiaDisplayControl.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace CUDA_Administrator.Controls
+{
+    partial class NvidiaDisplayControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbxFanSpeed = new System.Windows.Forms.ComboBox();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblClockSpeed = new System.Windows.Forms.Label();
+            this.notifyTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // cbxFanSpeed
+            // 
+            this.cbxFanSpeed.BackColor = System.Drawing.SystemColors.Control;
+            this.cbxFanSpeed.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxFanSpeed.FormattingEnabled = true;
+            this.cbxFanSpeed.Items.AddRange(new object[] {
+            "Default",
+            "Auto",
+            "0%",
+            "1%",
+            "2%",
+            "3%",
+            "4%",
+            "5%",
+            "6%",
+            "7%",
+            "8%",
+            "9%",
+            "10%",
+            "11%",
+            "12%",
+            "13%",
+            "14%",
+            "15%",
+            "16%",
+            "17%",
+            "18%",
+            "19%",
+            "20%",
+            "21%",
+            "22%",
+            "23%",
+            "24%",
+            "25%",
+            "26%",
+            "27%",
+            "28%",
+            "29%",
+            "30%",
+            "31%",
+            "32%",
+            "33%",
+            "34%",
+            "35%",
+            "36%",
+            "37%",
+            "38%",
+            "39%",
+            "40%",
+            "41%",
+            "42%",
+            "43%",
+            "44%",
+            "45%",
+            "46%",
+            "47%",
+            "48%",
+            "49%",
+            "50%",
+            "51%",
+            "52%",
+            "53%",
+            "54%",
+            "55%",
+            "56%",
+            "57%",
+            "58%",
+            "59%",
+            "60%",
+            "61%",
+            "62%",
+            "63%",
+            "64%",
+            "65%",
+            "66%",
+            "67%",
+            "68%",
+            "69%",
+            "70%",
+            "71%",
+            "72%",
+            "73%",
+            "74%",
+            "75%",
+            "76%",
+            "77%",
+            "78%",
+            "79%",
+            "80%",
+            "81%",
+            "82%",
+            "83%",
+            "84%",
+            "85%",
+            "86%",
+            "87%",
+            "88%",
+            "89%",
+            "90%",
+            "91%",
+            "92%",
+            "93%",
+            "94%",
+            "95%",
+            "96%",
+            "97%",
+            "98%",
+            "99%",
+            "100%"});
+            this.cbxFanSpeed.Location = new System.Drawing.Point(280, 1);
+            this.cbxFanSpeed.Name = "cbxFanSpeed";
+            this.cbxFanSpeed.Size = new System.Drawing.Size(50, 21);
+            this.cbxFanSpeed.TabIndex = 13;
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumber.ForeColor = System.Drawing.Color.White;
+            this.lblNumber.Location = new System.Drawing.Point(10, 5);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(13, 13);
+            this.lblNumber.TabIndex = 12;
+            this.lblNumber.Text = "1";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(35, 5);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(133, 13);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "NVIDIA GeForce GTX 780";
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.BackColor = System.Drawing.Color.Transparent;
+            this.lblTemp.ForeColor = System.Drawing.Color.White;
+            this.lblTemp.Location = new System.Drawing.Point(208, 5);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(36, 13);
+            this.lblTemp.TabIndex = 10;
+            this.lblTemp.Text = "100°C";
+            // 
+            // lblClockSpeed
+            // 
+            this.lblClockSpeed.AutoSize = true;
+            this.lblClockSpeed.BackColor = System.Drawing.Color.Transparent;
+            this.lblClockSpeed.ForeColor = System.Drawing.Color.White;
+            this.lblClockSpeed.Location = new System.Drawing.Point(364, 5);
+            this.lblClockSpeed.Name = "lblClockSpeed";
+            this.lblClockSpeed.Size = new System.Drawing.Size(29, 13);
+            this.lblClockSpeed.TabIndex = 11;
+            this.lblClockSpeed.Text = "MHz";
+            // 
+            // notifyTray
+            // 
+            this.notifyTray.Text = "CUDA Administrator";
+            this.notifyTray.Visible = true;
+            // 
+            // NvidiaDisplayControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.cbxFanSpeed);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.lblClockSpeed);
+            this.Name = "NvidiaDisplayControl";
+            this.Size = new System.Drawing.Size(400, 25);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.NvidiaDisplayControl_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxFanSpeed;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblClockSpeed;
+        private System.Windows.Forms.NotifyIcon notifyTray;
+    }
+}
---
+++ CUDA Administrator/Controls/NvidiaDisplayControl.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CUDA_Administrator.Data.Devices;
+using System.Drawing.Drawing2D;
+using CUDA_Administrator.Calls.Setting;
+using CUDA_Administrator.Data.Setting;
+
+namespace CUDA_Administrator.Controls
+{
+    public partial class NvidiaDisplayControl : UserControl
+    {
+        VideoCard nvidiaCard;
+        int TemperatureRun = 0;
+        bool IsCoolingDown = false;
+
+        public NvidiaDisplayControl()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.Transparent;
+        }
+
+        public void RefreshCard(VideoCard card)
+        {
+            string strError = string.Empty;
+            SettingCall settingsCall = new SettingCall();
+            Settings settings = settingsCall.GetSettings(ref strError); ;
+
+            nvidiaCard = card;
+
+            int temp = Convert.ToInt32(card.TemperatureSensors[0].Value);
+            Color clr = Color.White;
+            if (temp < 70)
+                clr = Color.Green;
+            else if (temp >= 70 && temp < 90)
+                clr = Color.Orange;
+            else if (temp >= 90)
+                clr = Color.Red;
+
+            lblNumber.Text = card.Number.ToString();
+            lblName.Text = card.Name;
+            lblTemp.Text = card.TemperatureSensors[0].Value.ToString() + "°C";
+            lblClockSpeed.Text = Convert.ToInt32(card.ClockSensors[0].Value).ToString() + " MHz";
+
+            lblTemp.ForeColor = clr;
+
+            if (!cbxFanSpeed.DroppedDown) //We don't want to add the fan % if this is dropped down because then the user will have a hard time selecting a new value
+                cbxFanSpeed.Text = card.FanSensors[0].Value.ToString() + "%";
+
+            if (settings.Options.IsTempProtectionActivated)
+            {
+                if (TemperatureRun > 0)
+                    TemperatureRun++;
+
+                if (card.TemperatureSensors[0].Value >= settings.Options.TemperatureMax && !IsCoolingDown)
+                {
+                    SendNotificationToTray("Temperature Protection", "Temperature threshold reached. The Fan will run at 100% for 3 minutes and then run at 50%.");
+                    nvidiaCard.FanSensors[0].Control.SetSoftware(100);
+                    IsCoolingDown = true;
+                }
+                else if (!settings.Options.IsTempProtectionActivated || card.TemperatureSensors[0].Value < settings.Options.TemperatureMax && TemperatureRun >= 180)  //180 = 3 minutes. Let the fan run for 3 minutes
+                {
+                    IsCoolingDown = false;
+                    TemperatureRun = 0;
+                    nvidiaCard.FanSensors[0].Control.SetSoftware(50);
+                }
+            }
+        }
+
+        public bool IsAdded(int num)
+        {
+            bool isadded = false;
+
+            if (Convert.ToInt32(lblNumber.Text) == num)
+                isadded = true;
+
+            return isadded;
+        }
+
+        private void NvidiaDisplayControl_Paint(object sender, PaintEventArgs e)
+        {
+            using (HatchBrush h1 = new HatchBrush(HatchStyle.ForwardDiagonal, UI.BackgroundColors.BackgroundDotsLight, UI.BackgroundColors.BackgroundColor))
+            {
+                e.Graphics.FillRectangle(h1, e.ClipRectangle);
+            }
+        }
+
+        private void cbxFanSpeed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbxFanSpeed.Text == "Auto")
+                nvidiaCard.FanSensors[0].Control.SetAuto();
+            else if (cbxFanSpeed.Text == "Default")
+                nvidiaCard.FanSensors[0].Control.SetDefault();
+            else
+                nvidiaCard.FanSensors[0].Control.SetSoftware(Convert.ToInt32(cbxFanSpeed.Text.Replace("%", "")));
+        }
+        private void SendNotificationToTray(string title, string message)
+        {
+            SettingCall settingsCall = new SettingCall();
+            notifyTray.BalloonTipTitle = title;
+            notifyTray.BalloonTipText = message;
+            notifyTray.Icon = settingsCall.GetApplicationIcon(); ;
+            notifyTray.ShowBalloonTip(5000);
+        }
+    }
+}
